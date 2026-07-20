5bb9a8da5194d241a0f859aa6eabba4ee4ea80b5
cassianasoares <cassianassilva@hotmail.com>
2026-07-20 19:54:10 -0300

    fix(ci): restrict pull request branches to staging and update test command path

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '.env')
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "ci_secret_key")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
